941dd511c92b4aa5acff821d403012b3f6c593a9
yashsolanki12 <yash@ecodesoft.com>
2026-03-26 18:28:35 +0530

    feat: Integrate app embed
    Setup theme extension for app embed
    
    Add starter code for .liquid file to show the toggle button
    
    Also add debug page to check the shopify app embed data
    
    Pass the embed data to list page of announcement to show or hide the fallback message accordingly

--- app/pages/announcement/index.jsx
+++ app/pages/announcement/index.jsx
@@ -3,11 +3,12 @@ import { getAllAnnouncement } from "../../api/announcement";
 import useAnnouncementData from "../../hooks/useAnnouncementData";
 import Loader from "../../ui/loader";
 
-const AnnouncementListPage = () => {
+const AnnouncementListPage = ({ appEmbedEnabled }) => {
   const { data: announcementListData, isLoading: announcementListLoading } =
     useAnnouncementData(["announcement"], getAllAnnouncement, null);
 
   console.log("Data:", announcementListData);
+  console.log("App embed", appEmbedEnabled);
 
   if (announcementListLoading) {
     return <Loader />;
