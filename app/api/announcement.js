e1a72c556924c27ab6391ab0eca7291e1f419c40
yashsolanki12 <yash@ecodesoft.com>
2026-03-27 10:53:59 +0530

    feat: Add fallback design for app embed & API setup
    Add new API file to fetch the current shop information.
    
    Use the backend localhost URL to implement that functionality.
    
    Pass the session data from index.jsx which is required while navigate to app embed.
    
    Create a function which can manage the navigate to app embed with session data and also test that in local development server.
    
    Show the card fallback only when app embed is false.
    
    Add the  announcement CRUD APIs function in the API folder of the web.

--- app/api/announcement.js
+++ app/api/announcement.js
@@ -6,7 +6,7 @@ export const getShopDomain = () => {
   return app;
 };
 
-// List API
+// List
 export const getAllAnnouncement = async () => {
   const shopDomain = getShopDomain();
 
@@ -25,5 +25,91 @@ export const getAllAnnouncement = async () => {
       const errorMessage = error.response?.data?.message || error.message;
       console.error("API Error in get all announcement:", errorMessage);
       throw new Error(errorMessage);
+    });
+};
+
+// Detail
+export const getAnnouncementById = async (id) => {
+  const shopDomain = getShopDomain();
+
+  if (!shopDomain) {
+    console.error("No shop domain found in URL parameters.");
+    throw new Error("Shop domain is required.");
+  }
+  return axiosInstance
+    .get(`announcement/${id}`, {
+      headers: {
+        "x-shopify-shop-domain": shopDomain,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      const errorMessage = error.response?.data?.message || error.message;
+      console.error("API Error in get announcement by id:", errorMessage);
+      throw new Error(errorMessage);
+    });
+};
+
+// Delete
+export const deleteAnnouncement = async (id) => {
+  const shopDomain = getShopDomain();
+
+  if (!shopDomain) {
+    console.error("No shop domain found un URL parameters.");
+    throw new Error("Shop domain is required");
+  }
+  return axiosInstance
+    .delete(`announcement/${id}`, {
+      headers: {
+        "x-shopify-shop-domain": shopDomain,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      const errorMessage = error.response?.data?.message || error.message;
+      console.error("API Error while delete announcement:", errorMessage);
+      throw new Error(errorMessage);
+    });
+};
+
+// Create
+export const createAnnouncement = async (data) => {
+  const shopDomain = getShopDomain();
+
+  if (!shopDomain) {
+    console.error("No shop domain found in URL parameters.");
+    throw new Error("Shop domain is required.");
+  }
+  return axiosInstance
+    .post("announcement/add", data, {
+      headers: {
+        "x-shopify-shop-domain": shopDomain,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.error("API Error in create announcement:", error);
+      throw error;
+    });
+};
+
+// Update API
+export const updateAnnouncement = async ({ id, data }) => {
+  const shopDomain = getShopDomain();
+
+  if (!shopDomain) {
+    console.error("No shop domain found in URL parameters.");
+    throw new Error("Shop domain is required.");
+  }
+  return axiosInstance
+    .put(`announcement/${id}`, data, {
+      headers: {
+        "x-shopify-shop-domain": shopDomain,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.error("API Error in update announcement:", error);
+      throw error;
     });
 };
